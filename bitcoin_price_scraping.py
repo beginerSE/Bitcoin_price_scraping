f54465927447120cc84f7ada892578c0687023a3
零細SE <42573936+beginerSE@users.noreply.github.com>
2019-10-25 10:28:31 +0900

    記法に合わせた形に修正

--- bitcoin_price_scraping.py
+++ bitcoin_price_scraping.py
@@ -7,26 +7,26 @@ import json
 import pandas as pd
 
 def get_btcprice(ticker,_max):
-    url=('https://api.coingecko.com/api/v3/coins/')+ticker+('/market_chart?vs_currency=jpy&days=')+_max
-    r=requests.get(url)
+    url='https://api.coingecko.com/api/v3/coins/' + ticker + '/market_chart?vs_currency=jpy&days=' +_max
+    r = requests.get(url)
     r2 = json.loads(r.text)
     return r2
 
 
 #jsonから価格データだけをPandasに変換して抽出する
 def get_price(r2):
-    s=pd.DataFrame(r2['prices'])
-    s.columns=['date','price']
-    date=[]
-    for i in s['date']:
-        tsdate=int(i/1000)
-        loc =datetime.utcfromtimestamp(tsdate)
+    data = pd.DataFrame(r2['prices'])
+    data.columns = ['date','price']
+    date = []
+    for i in data['date']:
+        tsdate = int(i / 1000)
+        loc = datetime.utcfromtimestamp(tsdate)
         date.append(loc)
-    s.index=date
-    del s['date']
-    return s
+    data.index = date
+    del data['date']
+    return data
 
 #ビットコインの全期間の価格データを取得する
-r2=get_btcprice('bitcoin','max')
-btcprice=get_price(r2)
+r2 = get_btcprice('bitcoin', 'max')
+btcprice = get_price(r2)
 btcprice.head()
