cf0c590b5f3ad8bd74f3255431fdbd48539d13be
bessho <oruka2365@gmail.com>
2019-12-06 10:56:53 +0900

    コードをきれいにしました

--- bitcoin_price_scraping.py
+++ bitcoin_price_scraping.py
@@ -7,28 +7,29 @@ import json
 
 
 def get_btcprice(ticker, _max):
+    """[CoingeckoのAPIからビットコインの価格を取得する関数]
+
+    Arguments:
+        ticker {[Sting]} -- [通貨のティッカー例：bitcoin,ethereum]
+        _max {[String]} -- [期間例：max,14days]
+
+    Returns:
+        [Dataframe] -- [価格と日付データを格納したデータフレーム]
+    """
+
     url = 'https://api.coingecko.com/api/v3/coins/' + \
         ticker + '/market_chart?vs_currency=jpy&days=' + _max
     r = requests.get(url)
     r2 = json.loads(r.text)
-    return r2
 
-
-
-def get_price(r2):
     data = pd.DataFrame(r2['prices'])
     data.columns = ['date', 'price']
-    date = []
-    for i in data['date']:
-        tsdate = int(i / 1000)
-        loc = datetime.utcfromtimestamp(tsdate)
-        date.append(loc)
+    date = [datetime.utcfromtimestamp(int(i / 1000)) for i in data['date']]
     data.index = date
     del data['date']
     return data
 
 
 # ビットコインの全期間の価格データを取得する
-r2 = get_btcprice('bitcoin', 'max')
-btcprice = get_price(r2)
-btcprice.head()
+btcprice = get_btcprice('bitcoin', 'max')
+print(btcprice.head())
